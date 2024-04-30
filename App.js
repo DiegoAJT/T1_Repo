1a9cc0a717c88aaf12ba7bb907b21c97157e0b41
DiegoAJT <N00293901@upn.pe>
2024-04-30 00:18:24 -0500

    Subiendo cambios de campo de estudiante

---
+++ App.js
@@ -0,0 +1,126 @@
+import React, { useReducer, useState } from 'react';
+import './App.css';
+
+const formReducer = (state, event) => {
+ return {
+   ...state,
+   [event.name]: event.value
+ }
+}
+
+function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [formData2, setFormData2] = useReducer(formReducer, {});
+  const [formData3, setFormData3] = useReducer(formReducer, {});
+  const [formData4, setFormData4] = useReducer(formReducer, {});
+  const [formData5, setFormData5] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 5000);
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange2 = event => {
+    setFormData2({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange3 = event => {
+    setFormData3({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange4 = event => {
+    setFormData4({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange5 = event => {
+    setFormData5({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+  return(
+    <div className="estilo">
+      <h1>React Formularios - Estudiante</h1>
+      {submitting &&
+       <div>
+         Mostrando registro:
+         <ul>
+           {Object.entries(formData).map(([name, value]) => (
+             <li key={name}><strong>Nombres</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(formData2).map(([name, value]) => (
+             <li key={name}><strong>Apellidos</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(formData3).map(([name, value]) => (
+             <li key={name}><strong>Fecha de Nacimiento</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(formData4).map(([name, value]) => (
+             <li key={name}><strong>Direcciòn</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(formData5).map(([name, value]) => (
+             <li key={name}><strong>Teléfono</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+       </div>
+      }
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Nombres</p>
+            <input name="name" onChange={handleChange}/>
+          </label>
+          <label>
+            <p>Apellidos</p>
+            <input name="ape" onChange={handleChange2}/>
+          </label>
+          <label>
+            <p>Fecha de Nacimiento</p>
+            <input name="fecnac" onChange={handleChange3}/>
+          </label>
+          <label>
+            <p>Dirección</p>
+            <input name="direc" onChange={handleChange4}/>
+          </label>
+          <label>
+            <p>Teléfono</p>
+            <input name="tel" onChange={handleChange5}/>
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+
+
+export default App;
